28500adfa518a6213619280bbe7a09ce912f175a
Ubuntu <ubuntu@ip-172-31-29-197.ap-southeast-1.compute.internal>
2018-03-21 05:37:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,36 @@ const port =  3000;
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const FILENAME = 'count.txt';
+const ERRORS_COUNT_FILE = 'errors_count.txt';
+const STOCK_COUNT_FILE = 'stock_count.txt';
 
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.get('/status', (req, res) => {
+app.get('/error_status', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  readCount()
+  readCount(ERRORS_COUNT_FILE)
   .then(number => res.json({ server_status: 'Up', number_of_errors: number, ip: ip }) )
   .catch(e => res.json({ server_status: 'Up but read count error', error: e, ip: ip }));
 });
+
+app.get('/stock_status', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  readCount(STOCK_COUNT_FILE)
+  .then(number => res.json({ server_status: 'Up', number_of_errors: number, ip: ip }) )
+  .catch(e => res.json({ server_status: 'Up but read count error', error: e, ip: ip }));
+});
+
 
 app.post('/report', (req, res) => {
   new Promise((resolve, reject) => {    
     const { key, error } = req.body;
     
-    fs.appendFile('errors.csv', `${key},${error}\r\n`, err => {
+    fs.appendFile('errors.csv', `${key},${error},${new Date()}\r\n`, err => {
       if (err) reject(err);
       
-      readCount()
-      .then(count)
+      readCount(ERRORS_COUNT_FILE)
+      .then(count(ERRORS_COUNT_FILE))
       .catch(e => reject(e));
 
       resolve(`${key},${error} saved!\r\n`);
@@ -38,13 +47,13 @@ app.post('/report', (req, res) => {
 
 app.post('/report_stock', (req, res) => {
   new Promise((resolve, reject) => {    
-    const { id, warehouse_soldable } = req.body;
+    const { id, warehouse_soldable, warehouse_soldable_before, stock_soldable } = req.body;
     
-    fs.appendFile('stock.csv', `${id},${warehouse_soldable}\r\n`, err => {
+    fs.appendFile('stock.csv', `${id}, ${warehouse_soldable_before}, ${warehouse_soldable}, ${stock_soldable}, ${new Date()}\r\n`, err => {
       if (err) reject(err);
       
-      readCount()
-      .then(count)
+      readCount(STOCK_COUNT_FILE)
+      .then(count(STOCK_COUNT_FILE))
       .catch(e => reject(e));
 
       resolve(`${id},${warehouse_soldable} saved!\r\n`);
@@ -60,10 +69,10 @@ app.listen(port, () => {
   console.log(`app listen on port ${port}`);
 });
 
-const count = (number) => {
+const count = (filename) => (number) => {
   return new Promise((resolve, reject) => {
     const newNumber = number + 1;
-    fs.writeFile(FILENAME, newNumber, e => {
+    fs.writeFile(filename, newNumber, { flag: 'w' }, e => {
       if (e) reject(e)
        
       resolve('count succeded!');
@@ -71,12 +80,13 @@ const count = (number) => {
   });
 }
 
-const readCount = () => {
+const readCount = (filename) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(FILENAME, (err, data) => {
+    fs.readFile(filename, (err, data) => {
       if (err) reject(err)
 
-      const number = parseInt(data.toString());
+      const newData = data || new Buffer(0);
+      const number = parseInt(newData.toString());
 
       resolve(number);
     });
